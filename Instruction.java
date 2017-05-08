b60bac2426f74b67461e1c7e6bc1590cc6636067
louballad12 <jfballad@up.edu.ph>
2017-05-09 04:42:54 +0800

    Add files via upload

--- Instruction.java
+++ Instruction.java
@@ -7,11 +7,13 @@ public class Instruction {
 	int[] registers = new int[2];
 	String instruction;
 	int immediateValue;
-	boolean raw;
-	boolean war;
-	boolean waw;
+	String stage = null;
+	boolean raw=false;
+	boolean war=false;
+	boolean waw=false;
+	
 
-	public Instruction(String inst, int valA, int valB) {
+	public Instruction(String inst, int valA, int valB) { //constructor
 		this.instruction = inst;
 		this.registers[0] = valA;
 
@@ -26,16 +28,134 @@ public class Instruction {
 		}
 	}
 
-	public void loadInstruction() {
+	public void loadInstruction(int index) {
+		
+			/*	if(this.stage == null){
+				this.stage =="F";
+				return ;
+				} */ 
 			
+			for (int i=0;i<index;i++) {
+				if(this.stage == ARMS.table[i][cpuCycle] ){
+				
+				/*	//raw
+					if (this.registers[2] == ) {
+						this.stage = "S";
+					//war
+					} else if (this.registers[2] == ) {
+					//waw
+					} else if (this.registers[1] ==) 
+				*/
+					break;
+				}
+			}
+		
+			if(this.immediateValue > 99 || this.immediateValue  < -99){
+					//sets the OF to 1
+					ARMS.otherRegisters[3] = 1;
+							
+			}else{
+					//sets the OF to 0
+					ARMS.otherRegisters[3] = 0;
+			}
+			
+		
+		
+			 if(this.stage == "E"){
+			this.registers[0] = this.immediateValue; 
+			}
+		
 	}
 
-	public void addInstruction() {
+	public void addInstruction(int index) {
+	
+				for (int i=0;i<index;i++) {
+					if(this.stage == ARMS.table[i][cpuCycle] ){
+						break;
+					}
+			}
+			
+			if(this.immediateValue > 99 || this.immediateValue < -99){
+					//sets the OF to 1
+					ARMS.otherRegisters[3] = 1;
+			}else{
+			 		//sets the OF to 0
+			 		ARMS.otherRegisters[3] = 0;
+			}	
+			
+			
+			if(this.stage =="E"){
+			this.registers[0] = this.registers[0] + this.registers[1];
+			}
+		
 	}
 
-	public void subInstruction() {
+	public void subInstruction(int index) {
+	
+				for (int i=0;i<index;i++) {
+					if(this.stage == ARMS.table[i][cpuCycle] ){
+					break;
+				}
+			}
+		
+			if(this.immediateValue > 99 || this.immediateValue < -99){
+					//sets the OF to 1
+					ARMS.otherRegisters[3] = 1;
+			}else{
+					//sets the OF to 0
+					ARMS.otherRegisters[3] = 0;
+			}	
+			
+			if(this.stage == "E"){
+			this.registers[0] = this.registers[0] - this.registers[1];
+			}
 	}
 
-	public void cmpInstruction() {
+	public void cmpInstruction(int index) {
+	
+			int difference;
+			
+				for (int i=0;i<index;i++) {
+					if(this.stage == ARMS.table[i][cpuCycle] ){
+						break;
+					}
+			}
+			
+			if(this.immediateValue > 99 || this.immediateValue < -99){
+					//sets the OF to 1
+					ARMS.otherRegisters[3] = 1;
+			}else{
+						//sets the OF to 0
+					ARMS.otherRegisters[3] = 0;
+			}
+			
+				
+			if(this.stage == "E"){
+			
+						difference = this.registers[0] - this.registers[1];
+			
+						if(difference == 0){
+							//sets ZF to 1
+								ARMS.otherRegisters[5] = 1;
+						}else{
+							//sets ZF to 0
+							 	ARMS.otherRegisters[5] = 0;
+						}
+			
+						if(difference < 0){
+							//sets NF to 1
+								ARMS.otherRegisters[4] = 1;
+						}else{
+							//sets NF to 0
+							ARMS.otherRegisters[4] = 0;
+						}
+			
+						if(difference > 0){
+								ARMS.otherRegisters[4] = 0;
+								ARMS.otherRegisters[5] = 0;
+						}
+			
+				}
+				
 	}
 }
